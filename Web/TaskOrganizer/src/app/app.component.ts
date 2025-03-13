25fd79acc0aeca6e41eb5391edff826d30932a0b
Jaafar Kaoussarani <jaafar.kaoussarani@cae.com>
2025-03-13 10:14:53 -0400

    add taiga

--- Web/TaskOrganizer/src/app/app.component.ts
+++ Web/TaskOrganizer/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { TuiRoot } from "@taiga-ui/core";
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginPageComponent } from "./features/login-page/login-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPageComponent],
+  imports: [RouterOutlet, LoginPageComponent, TuiRoot],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
