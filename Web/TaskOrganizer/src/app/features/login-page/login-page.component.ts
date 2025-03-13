6b0f703e49db4977781a220f23ad981aecdb5859
ErgoProxy1 <jaafar.kaoussarani@polymtl.ca>
2025-03-12 21:03:58 -0400

    error handling

--- Web/TaskOrganizer/src/app/features/login-page/login-page.component.ts
+++ Web/TaskOrganizer/src/app/features/login-page/login-page.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDividerModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDividerModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPageComponent {
+  private fb = inject(FormBuilder);
+
+  protected loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  })
 
   protected login(): void {
-    console.log("login")
+    console.log(this.loginForm.controls.email.invalid)
   }
 }
