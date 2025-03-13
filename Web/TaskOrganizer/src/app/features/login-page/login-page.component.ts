bef143500e697f717628b9019a6faad3aec88bad
ErgoProxy1 <jaafar.kaoussarani@polymtl.ca>
2025-03-12 22:58:29 -0400

    commit

--- Web/TaskOrganizer/src/app/features/login-page/login-page.component.ts
+++ Web/TaskOrganizer/src/app/features/login-page/login-page.component.ts
@@ -5,6 +5,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { take } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login-page',
@@ -14,7 +17,14 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPageComponent {
+  private readonly firebaseApiKey = "AIzaSyCWbHn1uBiby3RPHRKnvQHuXn4ld7SwAn0"; // Not a secret
+  //private readonly signInUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + this.firebaseApiKey;
+  private readonly signInUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=a"
+
   private fb = inject(FormBuilder);
+  private http = inject(HttpClient);
+
+  private snackBar = inject(MatSnackBar);
 
   protected loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -22,6 +32,20 @@ export class LoginPageComponent {
   })
 
   protected login(): void {
-    console.log(this.loginForm.controls.email.invalid)
+    let requestBody = {
+      email: this.loginForm.controls.email.value,
+      password: this.loginForm.controls.password.value,
+      returnSecureToken: true
+    }
+
+    let headers = {headers: new HttpHeaders({'Content-Type': 'application/json; charset=utf-8'})};
+    this.http.post(this.signInUrl, JSON.stringify(requestBody), headers).pipe(take(1)).subscribe(
+      (response) => { 
+        console.log(response)
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error)
+      } 
+    )
   }
 }
