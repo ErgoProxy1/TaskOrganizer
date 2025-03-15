cf4db0961f689774c10c36fdff465c86c12a83ad
ErgoProxy1 <jaafar.kaoussarani@polymtl.ca>
2025-03-14 21:46:25 -0400

    fix styling

--- Web/TaskOrganizer/src/app/app.component.ts
+++ Web/TaskOrganizer/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { TuiRoot } from "@taiga-ui/core";
+import { TuiRoot } from '@taiga-ui/core';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { LoginPageComponent } from "./features/auth-feature/login-page/login-page.component";
+import { LoginPageComponent } from './features/auth-feature/login-page/login-page.component';
+import { AuthContainerComponent } from './features/auth-feature/auth-container/auth-container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginPageComponent, TuiRoot],
+  imports: [LoginPageComponent, TuiRoot, AuthContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- Web/TaskOrganizer/src/app/features/auth-feature/auth-container/auth-container.component.ts
+++ Web/TaskOrganizer/src/app/features/auth-feature/auth-container/auth-container.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { LoginPageComponent } from '../login-page/login-page.component';
-import { SignupPageComponent } from "../signup-page/signup-page.component";
+import { SignupPageComponent } from '../signup-page/signup-page.component';
+import { TuiCardLarge } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-auth-container',
-  imports: [LoginPageComponent, SignupPageComponent],
+  imports: [LoginPageComponent, SignupPageComponent, TuiCardLarge],
   templateUrl: './auth-container.component.html',
-  styleUrl: './auth-container.component.scss'
+  styleUrl: './auth-container.component.scss',
 })
 export class AuthContainerComponent {
   protected currentPage: 'login' | 'signup' = 'login';
