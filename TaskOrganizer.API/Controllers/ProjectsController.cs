1d2431366a90f7b1d0d1e70f7bbaf6835402c6c5
ErgoProxy1 <jafk1998@gmail.com>
2025-03-26 00:53:32 -0400

    Refactor ProjectsController to implement JWT authentication and improve user identity validation

--- TaskOrganizer.API/Controllers/ProjectsController.cs
+++ TaskOrganizer.API/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
-﻿using Google.Api;
+﻿using FirebaseAdmin.Auth;
+using Google.Api;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -26,13 +27,16 @@ namespace TaskOrganizer.API.Controllers
     {
       try
       {
-        if (!string.IsNullOrWhiteSpace(userId))
+        var authorizationHeader = Request?.Headers?.Authorization.ToString();
+        if (authorizationHeader != null && !(await isAuthenticatedUser(authorizationHeader, userId)))
         {
+
           var query = _dbContext.Users.Where(u => u.Uid.Equals(userId)).SelectMany(u => u.UserProjects).Select(up => up.Project);
           var projects = await query.ToListAsync<Project>();
           return Ok(projects);
         }
-        throw new Exception("User Id unspecified");
+        return Forbid("User identity did not match request identity");
+
       }
       catch (Exception ex)
       {
@@ -40,29 +44,29 @@ namespace TaskOrganizer.API.Controllers
       }
     }
 
-    [HttpGet("{projectId}")]
+    //[HttpGet("{projectId}")]
     //TODO
-    public async Task<IActionResult> Get(string projectId, [FromQuery] string userId)
-    {
-      try
-      {
-        Guid parsedId;
-        if (projectId != null && Guid.TryParse(projectId, out parsedId))
-        {
-          var project = await _dbContext.Projects.FirstAsync(project => project.Id.Equals(parsedId));
-          if (project != null)
-          {
-            return Ok(project);
-          }
-          return NotFound(new { Error = "Project could not be found" });
-        }
-        return StatusCode(500, new { Error = "Project ID was null or invalid" });
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(new { Error = ex.Message });
-      }
-    }
+    //public async Task<IActionResult> Get(string projectId, [FromQuery] string userId)
+    //{
+    //  try
+    //  {
+    //    Guid parsedId;
+    //    if (projectId != null && Guid.TryParse(projectId, out parsedId))
+    //    {
+    //      var project = await _dbContext.Projects.FirstAsync(project => project.Id.Equals(parsedId));
+    //      if (project != null)
+    //      {
+    //        return Ok(project);
+    //      }
+    //      return NotFound(new { Error = "Project could not be found" });
+    //    }
+    //    return StatusCode(500, new { Error = "Project ID was null or invalid" });
+    //  }
+    //  catch (Exception ex)
+    //  {
+    //    return BadRequest(new { Error = ex.Message });
+    //  }
+    //}
 
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] ProjectDTO request)
@@ -75,20 +79,25 @@ namespace TaskOrganizer.API.Controllers
           return NotFound(new { Error = "User not found" });
         }
 
-        var project = new Project
+        var authorizationHeader = Request?.Headers?.Authorization.ToString();
+        if (authorizationHeader != null && !(await isAuthenticatedUser(authorizationHeader, user.Uid)))
         {
-          Id = Guid.NewGuid(),
-          Name = request.Name,
-          CreatedByUid = request.CreatedByUid,
-          Description = request.Description,
-        };
-        project.UserProjects.Add(new UserProject { Project = project, User = user });
+          var project = new Project
+          {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            CreatedByUid = request.CreatedByUid,
+            Description = request.Description,
+          };
+          project.UserProjects.Add(new UserProject { Project = project, User = user });
 
-        _dbContext.Projects.Add(project);
+          _dbContext.Projects.Add(project);
 
-        await _dbContext.SaveChangesAsync();
+          await _dbContext.SaveChangesAsync();
 
-        return Ok(project);
+          return Ok(project);
+        }
+        return Forbid("User identity did not match request identity");
       }
       catch (Exception ex)
       {
@@ -97,27 +106,36 @@ namespace TaskOrganizer.API.Controllers
     }
 
     [HttpPut("{projectId}")]
-    //TODO
-    public async Task<IActionResult> Put(string projectId, [FromBody] ProjectDTO request)
+    public async Task<IActionResult> Put([FromQuery] string projectId, [FromQuery] string userId, [FromBody] ProjectDTO request)
     {
       try
       {
-        Guid parsedId;
-        if (Guid.TryParse(projectId, out parsedId))
+        var user = await _dbContext.Users.Include(u => u.UserProjects).FirstOrDefaultAsync(u => u.Uid == request.CreatedByUid);
+        if (user == null)
         {
-          var project = new Project
-          {
-            Id = parsedId,
-            Name = request.Name,
-            CreatedByUid = request.CreatedByUid,
-            Description = request.Description,
-          };
-
-          this._dbContext.Projects.Update(project);
-          await _dbContext.SaveChangesAsync();
-          return Ok(project);
+          return NotFound(new { Error = "User not found" });
         }
-        return StatusCode(500, new { Error = $"Project with Id {projectId} does not exist" });
+        var authorizationHeader = Request?.Headers?.Authorization.ToString();
+        if (authorizationHeader != null && !(await isAuthenticatedUser(authorizationHeader, userId)))
+        {
+          Guid parsedId;
+          if (Guid.TryParse(projectId, out parsedId))
+          {
+            var project = new Project
+            {
+              Id = parsedId,
+              Name = request.Name,
+              CreatedByUid = request.CreatedByUid,
+              Description = request.Description,
+            };
+
+            this._dbContext.Projects.Update(project);
+            await _dbContext.SaveChangesAsync();
+            return Ok(project);
+          }
+          return NotFound(new { Error = $"Project with Id {projectId} does not exist" });
+        }
+        return Forbid("User identity did not match request identity");
       }
       catch (Exception ex)
       {
@@ -125,30 +143,37 @@ namespace TaskOrganizer.API.Controllers
       }
     }
 
-    [HttpDelete("{projectId}")]
+    //[HttpDelete("{projectId}")]
     //TODO
-    public async Task<IActionResult> Delete(string projectId)
+    //public async Task<IActionResult> Delete(string projectId)
+    //{
+    //  try
+    //  {
+    //    Guid parsedId;
+    //    if (projectId != null && Guid.TryParse(projectId, out parsedId))
+    //    {
+    //      var project = await this._dbContext.Projects.FirstAsync(p => p.Id.Equals(parsedId));
+    //      if (project == null)
+    //      {
+    //        return NotFound(new { Error = $"Project with Id {projectId} does not exist" });
+    //      }
+    //      this._dbContext.Projects.Remove(project);
+    //      await _dbContext.SaveChangesAsync();
+    //      return Ok();
+    //    }
+    //    return StatusCode(500, new { Error = $"Project with Id {projectId} does not exist" });
+    //  }
+    //  catch (Exception ex)
+    //  {
+    //    return BadRequest(new { Error = ex.Message });
+    //  }
+    //}
+
+    private async Task<bool> isAuthenticatedUser(string jwt, string userId)
     {
-      try
-      {
-        Guid parsedId;
-        if (projectId != null && Guid.TryParse(projectId, out parsedId))
-        {
-          var project = await this._dbContext.Projects.FirstAsync(p => p.Id.Equals(parsedId));
-          if (project == null)
-          {
-            return NotFound(new { Error = $"Project with Id {projectId} does not exist" });
-          }
-          this._dbContext.Projects.Remove(project);
-          await _dbContext.SaveChangesAsync();
-          return Ok();
-        }
-        return StatusCode(500, new { Error = $"Project with Id {projectId} does not exist" });
-      }
-      catch (Exception ex)
-      {
-        return BadRequest(new { Error = ex.Message });
-      }
+      FirebaseToken decodedToken = await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(jwt.Split(" ")[1]);
+      string decodedUid = decodedToken.Uid;
+      return decodedUid.Equals(userId);
     }
   }
 }
