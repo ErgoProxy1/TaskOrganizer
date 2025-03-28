53e4c6ff939d5e0e0eb371bb3b11a7a09912edb3
ErgoProxy1 <jafk1998@gmail.com>
2025-03-27 23:30:39 -0400

    dialogue

---
+++ Web/TaskOrganizer/src/app/features/projects-feature/new-project/new-project.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-project',
+  imports: [],
+  templateUrl: './new-project.component.html',
+  styleUrl: './new-project.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NewProjectComponent {
+
+}
--- Web/TaskOrganizer/src/app/features/projects-feature/project-header/project-header.component.ts
+++ Web/TaskOrganizer/src/app/features/projects-feature/project-header/project-header.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
 import { AvatarComponent } from '../../../core/components/avatar/avatar.component';
-import { TuiButton, TuiIcon } from '@taiga-ui/core';
+import { TuiButton, tuiDialog, TuiDialogService, TuiIcon } from '@taiga-ui/core';
+import { NewProjectComponent } from '../new-project/new-project.component';
+import { take } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-project-header',
@@ -8,4 +11,18 @@ import { TuiButton, TuiIcon } from '@taiga-ui/core';
   templateUrl: './project-header.component.html',
   styleUrl: './project-header.component.scss',
 })
-export class ProjectHeaderComponent {}
+export class ProjectHeaderComponent {
+  private readonly newProjectDialog = tuiDialog(NewProjectComponent, { dismissible: false });
+
+  private dialogRef = inject(TuiDialogService);
+  private destroyRef = inject(DestroyRef);
+  constructor() {}
+
+  protected openNewProjectDialog(): void {
+    this.newProjectDialog()
+      .pipe(take(1), takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => {
+        console.log('done');
+      });
+  }
+}
