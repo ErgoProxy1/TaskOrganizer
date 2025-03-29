211748c6265cd5db3a2cefd0389345dafee3ed64
ErgoProxy1 <jafk1998@gmail.com>
2025-03-28 23:35:05 -0400

    form

--- TaskOrganizer.API/Models/Project.cs
+++ TaskOrganizer.API/Models/Project.cs
@@ -3,19 +3,20 @@ using System.Text.Json.Serialization;
 
 namespace TaskOrganizer.API.Models
 {
-    public class Project
-    {
-        [Key]
-        public required Guid Id { get; set; }
-        [Required]
-        public required string Name { get; set; }
-        [MaxLength(400)]
-        public string Description { get; set; } = "";
-        [Required]
-        public required string CreatedByUid { get; init; }
+  public class Project
+  {
+    [Key]
+    public required Guid Id { get; set; }
+    [Required]
+    [MaxLength(60)]
+    public required string Name { get; set; }
+    [MaxLength(400)]
+    public string Description { get; set; } = "";
+    [Required]
+    public required string CreatedByUid { get; init; }
 
-        // Navigation
-        [JsonIgnore]
-        public List<UserProject> UserProjects { get; } = [];
-    }
+    // Navigation
+    [JsonIgnore]
+    public List<UserProject> UserProjects { get; } = [];
+  }
 }
--- Web/TaskOrganizer/src/app/features/projects-feature/new-project/new-project.component.ts
+++ Web/TaskOrganizer/src/app/features/projects-feature/new-project/new-project.component.ts
@@ -1,12 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { TuiButton, TuiError, TuiTextfield, TuiTitle, TuiAppearance, TuiLoader } from '@taiga-ui/core';
+import { TuiHeader } from '@taiga-ui/layout';
+import { TuiInputModule, TuiTextareaModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'app-new-project',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    TuiButton,
+    TuiError,
+    TuiTextfield,
+    TuiTitle,
+    TuiAppearance,
+    TuiHeader,
+    MatDividerModule,
+    TuiLoader,
+    TuiTextareaModule,
+    TuiInputModule,
+    TuiTextfieldControllerModule,
+  ],
   templateUrl: './new-project.component.html',
   styleUrl: './new-project.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NewProjectComponent {
-
+  private fb = inject(FormBuilder);
+  protected projectForm = this.fb.group({
+    title: ['', [Validators.required, Validators.maxLength(60)]],
+    description: ['', [Validators.maxLength(400)]],
+  });
 }
